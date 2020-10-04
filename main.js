1de48cfce8b5c38f66874f61ceba47c283840cae
Fedaa <fadooshjad2001@gmail.com>
2020-10-04 23:37:09 +0300

    Fedaa-2

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-$("#save").click(function() {
-	var bookname=$("#book-name").val();
-	var auther=$("#auther-name").val();
-	var bookcover=$("#book-cover").val();
-	var quote=$("#quote").val();
-	var rate =$("#rate").val();
-	var ownerwindow = window.open()
-	// $("<UL id = "list"></UL>").appendTo(ownerwindow)
-	// $("<li>"+ownerwindow.document.write(bookname)+"</li>")
+$("#enter").click(function() {
 
-	//  $("<li>"+ownerwindow.document.write(rate)+"</li>")
-
-
+ //   var bookname=$("#book-name").val();
+	// var auther=$("#auther-name").val();
+	// var bookcover=$("#book-cover").val();
+	// var quote=$("#quote").val();
+	// var rate =$("#rate").val();
+	var url = "file:///C:/Users/F5/Desktop/project-1/two.html"
+	window.open(url)
+ownerwindow.document.write(bookname)
 
 
 });
+
+function counter(){
+	var c =0;
+	var input= document.createElement("input")
+
+}
